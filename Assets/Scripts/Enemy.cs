cc6aa0cf713cb4a05fd1cb16a3041d10f8220ba0
SungChoi <swc1098@rit.edu>
2016-02-08 04:52:31 -0500

    Random Enemy Type
    Used enums and set the material colors to whatever it needed to be.
    
    Enums are the only things that matter here, so Zach you can use this to check when you make your barriers.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,12 +9,25 @@ public class Enemy : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+        // Get the renderer
+        Renderer rend = GetComponent<Renderer>();
+
         isColliding = false;
         EnemyType type = GetRandomEnum();
 
+        // if statements will check the type, and then color the material accordingly.
         if(type == EnemyType.Blue)
         {
-
+            rend.material.SetColor("_Color", Color.blue);
+        }
+        else if(type == EnemyType.Green)
+        {
+            rend.material.SetColor("_Color", Color.green);
+        }
+        else if(type == EnemyType.Red)
+        {
+            rend.material.SetColor("_Color", Color.red);
         }
 }
 	
@@ -28,6 +41,7 @@ public class Enemy : MonoBehaviour {
     // If so, delete current gameObject and subtract one health.
     void OnTriggerEnter(Collider other)
     {
+        // check for the player tag. If it exists, set collision to true and then destroy existing gameobject
         if(other.tag == "Player")
         {
             
@@ -37,9 +51,12 @@ public class Enemy : MonoBehaviour {
         }
     }
 
+    // Gets a random enum by using the values existing within the enum
     static EnemyType GetRandomEnum()
     {
+        // create an array that holds the values of each EnemyType
         System.Array a = System.Enum.GetValues(typeof(EnemyType));
+        // cycle through the array and then return a random enum type
         EnemyType newEnemy = (EnemyType)a.GetValue(Random.Range(0, a.Length));
         return newEnemy;
     }
