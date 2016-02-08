5c76a0fde8d2e8b2b3e646bbf80ecb78c65cb2d0
SungChoi <swc1098@rit.edu>
2016-02-08 04:34:44 -0500

    Primitive Enemy Test
    Got started on enemies, and made sure they were somewhat functional. This is not indicative of the final product, of course.
    After this, I am currently working on making the enemy spawn random types based on enums. I'll see if I can get that done.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum EnemyType { Red = 1,Green = 2,Blue = 3};
+
+public class Enemy : MonoBehaviour {
+    // for testing purposes. Will not exist in final build.
+    public bool isColliding;
+
+	// Use this for initialization
+	void Start () {
+        isColliding = false;
+        EnemyType type = GetRandomEnum();
+
+        if(type == EnemyType.Blue)
+        {
+
+        }
+}
+	
+	// Update is called once per frame
+	void Update () {
+        // Very primitive, meant for testing.
+        transform.Translate(Vector3.down * Time.deltaTime);
+	}
+
+    // When sphere collider triggers, check if the collided object is a player.
+    // If so, delete current gameObject and subtract one health.
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            
+            isColliding = true;
+            Destroy(gameObject);
+
+        }
+    }
+
+    static EnemyType GetRandomEnum()
+    {
+        System.Array a = System.Enum.GetValues(typeof(EnemyType));
+        EnemyType newEnemy = (EnemyType)a.GetValue(Random.Range(0, a.Length));
+        return newEnemy;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    // Health, keeps track of player life. (5 default) 
+    public int Health = 5;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
