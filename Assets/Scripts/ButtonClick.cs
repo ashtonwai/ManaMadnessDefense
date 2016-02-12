accb87ce9972e324c7938f49aeb4907dce159fd0
Zach Bebel <zlb3507@rit.edu>
2016-02-12 13:19:12 -0500

    New layout structure and updated button function

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class ButtonClick : MonoBehaviour {
 
 	public string element;
 	public Button currentButton;
+	public Dictionary<Image, float> receivers = new Dictionary<Image, float>();
+	private Image toRemove;
 
 	public Button fireElement;
 	public Button waterElement;
@@ -14,6 +17,8 @@ public class ButtonClick : MonoBehaviour {
 	public int fireCount = 10;
 	public int waterCount = 10;
 	public int grassCount = 10;
+
+	public float timeVisible = 0.1f;
 
 	// Use this for initialization
 	void Start () {
@@ -24,7 +29,19 @@ public class ButtonClick : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
+		toRemove = null;
+		foreach (KeyValuePair<Image, float> receiver in receivers) {
+			if (receiver.Value > 0) {
+				receivers[receiver.Key] -= Time.deltaTime;
+			}
+			else if (receiver.Value <= 0) {
+				receiver.Key.GetComponent<Image> ().color = Color.white;
+				toRemove = receiver.Key;
+			}
+		}
+		if (toRemove != null) {
+			receivers.Remove (toRemove);
+		}
 	}
 
 	public void onClick(Button button) {
@@ -37,16 +54,19 @@ public class ButtonClick : MonoBehaviour {
 		case "Fire":
 			fireCount--;
 			image.GetComponent<Image> ().color = Color.red;
+			receivers.Add(image, timeVisible);
 			currentButton.GetComponentInChildren<Text> ().text = element + " " + fireCount;
 			break;
 		case "Water":
 			waterCount--;
 			image.GetComponent<Image> ().color = Color.blue;
+			receivers.Add(image, timeVisible);
 			currentButton.GetComponentInChildren<Text> ().text = element + " " + waterCount;
 			break;
 		case "Grass":
 			grassCount--;
 			image.GetComponent<Image> ().color = Color.green;
+			receivers.Add(image, timeVisible);
 			currentButton.GetComponentInChildren<Text> ().text = element + " " + grassCount;
 			break;
 		default:
