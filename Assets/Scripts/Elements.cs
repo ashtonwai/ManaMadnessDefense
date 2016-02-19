052f3b6d119ce0e8c4955603e3c8008cc6a7bf85
Ashton Wai <indexsky@users.noreply.github.com>
2016-02-19 03:09:13 -0500

    Add timer, and element, element box prefabs.

---
+++ Assets/Scripts/Elements.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Elements : MonoBehaviour {
+
+	public string element;
+	public Button currentButton;
+	public Dictionary<Image, float> receivers = new Dictionary<Image, float>();
+	private Image toRemove;
+
+	public Button fireElement;
+	public Button waterElement;
+	public Button grassElement;
+
+	public int fireCount = 10;
+	public int waterCount = 10;
+	public int grassCount = 10;
+
+	public float timeVisible = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		fireElement.GetComponentInChildren<Text> ().text = "Fire " + fireCount;
+		waterElement.GetComponentInChildren<Text> ().text = "Water " + waterCount;
+		grassElement.GetComponentInChildren<Text> ().text = "Grass " + grassCount;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		toRemove = null;
+		foreach (KeyValuePair<Image, float> receiver in receivers) {
+			if (receiver.Value > 0) {
+				receivers[receiver.Key] -= Time.deltaTime;
+			}
+			else if (receiver.Value <= 0) {
+				receiver.Key.GetComponent<Image> ().color = Color.white;
+				toRemove = receiver.Key;
+			}
+		}
+		if (toRemove != null) {
+			receivers.Remove (toRemove);
+		}
+	}
+
+	public void onClick(Button button) {
+		currentButton = button;
+		element = button.name;
+	}
+
+	public void setElement(Image image) {
+		switch (element) {
+		case "Fire":
+			fireCount--;
+			image.GetComponent<Image> ().color = Color.red;
+			receivers.Add(image, timeVisible);
+			currentButton.GetComponentInChildren<Text> ().text = element + " " + fireCount;
+			break;
+		case "Water":
+			waterCount--;
+			image.GetComponent<Image> ().color = Color.blue;
+			receivers.Add(image, timeVisible);
+			currentButton.GetComponentInChildren<Text> ().text = element + " " + waterCount;
+			break;
+		case "Grass":
+			grassCount--;
+			image.GetComponent<Image> ().color = Color.green;
+			receivers.Add(image, timeVisible);
+			currentButton.GetComponentInChildren<Text> ().text = element + " " + grassCount;
+			break;
+		default:
+			print ("no element");
+			break;
+		}
+
+		element = null;
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text timerText;
+	public float minutes = 2;
+	public float seconds = 0;
+
+	// Use this for initialization
+	void Start () {
+		timerText = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (seconds <= 0) {
+			seconds = 59;
+
+			if (minutes >= 1) {
+				minutes--;
+			} else {
+				minutes = 0;
+				seconds = 0;
+				timerText.text = minutes.ToString ("f0") + ":0" + seconds.ToString ("f0");
+			}
+		} else {
+			seconds -= Time.deltaTime;
+		}
+
+		if (Mathf.Round (seconds) <= 9) {
+			timerText.text = minutes.ToString ("f0") + ":0" + seconds.ToString ("f0");
+		} else {
+			timerText.text = minutes.ToString ("f0") + ":" + seconds.ToString ("f0");
+		}
+	}
+}
